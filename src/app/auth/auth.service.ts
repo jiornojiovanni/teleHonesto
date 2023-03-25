46da4f1db70071ebb44a6c066fbbe6d961e8a8a4
jiornojiovanni <gianni00palmieri@gmail.com>
2023-03-25 17:26:16 +0100

    save jwt token on local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,26 +3,37 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { Md5 } from 'ts-md5';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isLoggedIn = false;
-  private bearerToken = "";
-
   constructor(private httpClient: HttpClient, private router: Router) { }
 
   isAuthenticated() {
-    return this.isLoggedIn;
+    return moment().isBefore(this.getExpiresAt());
   }
 
   getToken() {
-    return this.bearerToken;
+    return localStorage.getItem("jwt_token");
   }
 
-  setToken(token: string) {
-    this.bearerToken = token;
+  private getExpiresAt() {
+    const expiration = localStorage.getItem("expires_at");
+    if(expiration != null) {
+      const expiresAt = JSON.parse(expiration);
+      return moment(expiresAt);
+    } else {
+      return null;
+    }
+    
+  }
+
+  setToken(token: string, expiresIn: number) {
+    const expiresAt = moment().add(expiresIn, 'second');
+    localStorage.setItem("jwt_token", token);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
   }
 
   getHeaderWithBearer() {
@@ -40,10 +51,8 @@ export class AuthService {
   }
 
   logout() {
-    this.isLoggedIn = false;
+    localStorage.removeItem("jwt_token");
+    localStorage.removeItem("expires_at");
   }
 
-  login() {
-    this.isLoggedIn = true;
-  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   email: any;
   password: any;
   showError: any = false;
 
   constructor(private authService: AuthService, private router: Router) {}
+ 
+  ngOnInit() {
+    if(this.authService.isAuthenticated()) {
+      this.router.navigate(['profile']);
+    }
+  }
 
   submit() {
     this.authService.auth(this.email, this.password).subscribe(resp => {
       if(resp.status == 200) {
-        this.authService.login();
-        this.authService.setToken(resp.body.token);
+        this.authService.setToken(resp.body.token, resp.body.expiresIn);
         this.router.navigate(['profile']);
       } else {
         this.showError = true;

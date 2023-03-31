1976dd68f902e8d9cfa9fbc99f5700523a94a3cf
Nicola Pomarico <nikomatri@gmail.com>
2023-03-31 16:34:07 +0200

    fixes ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { VideocallPJComponent } from './videocall-pj/videocall-pj.component';
 import { WebRTCComponent } from './web-rtc/web-rtc.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
   { path: 'pocPJ/:visitId', component: VideocallPJComponent, canActivate: [AuthGuard]},
   { path: 'WebRTC/:visitId', component: WebRTCComponent, canActivate: [AuthGuard]}
